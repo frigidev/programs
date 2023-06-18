3c87997fdebad10819a346ef1fea07fb331a3a46
Orestes Lucas <oresteslucas@gmail.com>
2023-06-18 18:57:56 -0300

    Update Calculator.js

--- Calculator.js
+++ Calculator.js
@@ -1,36 +1,34 @@
 class Calculator {
-
-    constructor(){
-        this.aux = 0
-    }
-
-    calculate(height, weight){
-      if(height, weight > 0){
-          this.aux = height * height
-          res = weight / this.aux
-          console.log("Your body mass index is: " + Math.floor(res))
-      }else{
-        console.log("Enter a valid entry")
-        }if(res <= 18.5 && res < 0){
-          console.log("Your weight is below normal")
-        }else if(res >= 18.5 && res <= 24.9){
-          console.log("You are at normal weight")
-        }else if(res >= 25 && res <= 29.9){
-          console.log("You are overweight")
-        }else if(res >= 30 && res <= 39.9){
-          console.log("You are obese")
-        }else if(res >= 40){
-          console.log("You are morbidly obese, try to improve your diet and seek medical advice")
-        }else{
-          console.log("Enter a valid entry")
+    calculateBMI(height, weight) {
+      if (height && weight > 0) {
+          const bmi = weight / (height * height);
+          alert("Your body mass index is: " + Math.floor(bmi));
+  
+          if(res <= 18.5) {
+          alert("Your weight is below normal")
+        } else if (bmi <= 24.9) {
+          alert("You are at normal weight")
+        } else if (bmi <= 29.9) {
+          alert("You are overweight")
+        } else if (bmi <= 39.9) {
+          alert("You are obese")
+        } else {
+          alert(
+              "You are morbidly obese, try to improve your diet and seek medical advice"
+          );
         }
+    } else {
+          alert("Enter valid values for height and weight.");
     }
+  }
 }
 
 const calculator = new Calculator()
 
-let height = 0
-let weight = 0
-let res = 0
-
-calculator.calculate()
+function getUserInput() {
+    const height = parseFloat(prompt("Enter your height in meters:"));
+    const weight = parseFloat(prompt("Enter your weight in kilograms:"));
+    calculator.calculateBMI(height, weight);
+}
+    
+getUserInput();
