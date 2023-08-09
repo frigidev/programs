560a54115c5efdc1e6ace11566f6947776b62625
Orestes Lucas <oresteslucas@gmail.com>
2023-08-09 09:07:57 -0300

    Criando Pessoa.java

---
+++ java_poo/sistema_poo_pratica/Pessoa.java
@@ -0,0 +1,15 @@
+package sistema_poo_pratica;
+
+public class Pessoa {
+    
+    private String nome;
+
+    protected void setNome(String nome) {
+        this.nome = nome; 
+    }
+
+    protected String getNome() {
+        return this.nome;
+    }
+    
+}
