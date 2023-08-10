bf233d79be8229ebdc6c7eb12d313a7bcf29b55e
Orestes Lucas <oresteslucas@gmail.com>
2023-08-09 21:19:33 -0300

    Atualizando AlunoEnsinoFundamental.java

--- java_poo/sistema_poo_pratica/AlunoEnsinoFundamental.java
+++ java_poo/sistema_poo_pratica/AlunoEnsinoFundamental.java
@@ -3,7 +3,7 @@ package sistema_poo_pratica;
 public class AlunoEnsinoFundamental extends Aluno {
 
     public double getMedia() {
-        return (getTeste() + getProva()) / 2;
+        return (getTeste() + getProva())/2;
     }   
 	
 }
