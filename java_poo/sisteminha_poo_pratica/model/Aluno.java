2ee6cb83e3d73258eea47723d90b9122268ac8ed
Orestes Lucas <oresteslucas@gmail.com>
2024-02-28 01:48:26 -0300

    Update and rename Student.java to Aluno.java

---
+++ java_poo/sisteminha_poo_pratica/model/Aluno.java
@@ -0,0 +1,23 @@
+package sistema_poo_pratica;
+
+public abstract class Aluno extends Pessoa implements Media {
+
+    private double prova, teste;
+	
+    protected void setProva(double prova) {
+        this.prova = prova;
+    }
+
+    protected double getProva() {
+        return this.prova;
+    }
+	
+    protected void setTeste(double teste) {
+        this.teste = teste;
+    }
+
+    protected double getTeste() {
+        return this.teste;
+    }
+    
+}
